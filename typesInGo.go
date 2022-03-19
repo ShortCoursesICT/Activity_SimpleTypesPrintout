0fe8e8b15e7d7a99e7609d9dbe3b7da26147fc8c
ShortCoursesICT <ict.cet.shortcourses@gmail.com>
2022-03-19 15:31:59 +0800

    Types in Go - no need test

---
+++ typesInGo.go
@@ -0,0 +1,11 @@
+package main
+
+//Insert variables declarations here based on activity
+
+func tellMeTypes() {
+	//insert code here to print out types of variables
+}
+
+func main() {
+	tellMeTypes()
+}
